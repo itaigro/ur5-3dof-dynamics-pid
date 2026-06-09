f257dcfbfe7bf338c6e6ad3c4a0d6b8bb3830010
Itaigro <itaigro@gmail.com>
2026-06-09 16:54:52 +0300

    Fix stick figure save: explicit Axes3D import for matplotlib
    Co-authored-by: Daniel <zionidan@post.bgu.ac.il>

--- src/ur5_3dof/visualization.py
+++ src/ur5_3dof/visualization.py
@@ -7,6 +7,7 @@ from typing import Optional
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.figure import Figure
 
 from .kinematics import forward_kinematics
